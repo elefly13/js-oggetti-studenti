72fec01082a7d0b4b2b9ea75482d3b15b86f3a0b
Elefly13 <elymara13@gmail.com>
2021-07-30 17:25:26 +0200

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -3,13 +3,20 @@
 let studente = {
     nome : 'Elena',
     cognome : 'Marangoni',
-    eta : 35
+    eta : 53
 }
+console.log(studente);
+ 
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
 for (let key in studente) {
-        document.getElementById('studente').innerHTML = studente['nome'] + ' ' + studente['cognome'];
+        
+        //inserisco gli elementi in una lista in html
+        let templateRiga = document.createElement('li');
+        templateRiga.classList.add("lista");
+        templateRiga.innerHTML = key + ': ' + studente[key];
+        document.getElementById('studente').appendChild(templateRiga);
     }
 // Creare un array di oggetti di studenti.
 let classeStudenti = [ 
@@ -39,20 +46,7 @@ let classeStudenti = [
         eta : 15
     }
 ];
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-
-for (let i = 0; i < classeStudenti.length; i++) {
-        console.log(classeStudenti[i])
-        let classe = classeStudenti[i];
-        for (let key in classeStudenti) {
-            console.log(classe['nome'] + ' ' + classe['cognome']);
-        //     document.getElementsByClassName('alunno').innerHTML = classe['nome'] + ' ' + classe['cognome'];
-        // }
-    }
-
-};
-
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+// // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 let newStudente = {
     nome : prompt('Inserisci il tuo nome'),
     cognome: prompt('Inserisci il tuo cognome'),
@@ -60,4 +54,24 @@ let newStudente = {
 }
 classeStudenti.push(newStudente);
 console.log(classeStudenti);
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+for (let i = 0; i < classeStudenti.length; i++) {
+    console.log(classeStudenti[i])
+    let classe = classeStudenti[i];
+    
+    console.log(classe['nome'] + ' ' + classe['cognome']);
+
+    //inserisco gli elementi in una lista in html
+    let templateRiga = document.createElement('li');
+    templateRiga.classList.add("lista");
+    templateRiga.innerHTML = classe['nome'] + ' ' + classe['cognome'];
+    document.getElementById('elenco').appendChild(templateRiga);
+
+    
+
+};
+
+
 
